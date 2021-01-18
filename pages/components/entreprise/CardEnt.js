15e69a33c92a034cf1c41d57bc89b2ed85dd1f4d
OnceLearner <redaamir972@gmail.com>
2021-01-18 21:14:43 +0100

    test :test2

--- pages/components/entreprise/CardEnt.js
+++ pages/components/entreprise/CardEnt.js
@@ -15,51 +15,51 @@ const deleteentreprise = id_entreprise => {
 }
 
 
-const CardEnt = (props) => {
+const CardEnt = ({ entreprise }) => {
     const [isHover, setIsHover] = useState(false)
     return (
         <div className="flex flex-col   border border-gray-600 shadow-xl hover:shadow-2xl p-4  md:w-80 flex-wrap rounded-lg space-y-4 bg-gradient-to-tr from-gray-800 to-gray-700  transform cursor-pointer hover:scale-105 transition delay-50  " style={{ fontFamily: "Questrial" }} onMouseEnter={() => setIsHover(true)} onMouseLeave={() => setIsHover(false)} >
             <div className="flex gap-8 items-center justify-center p-3">
-                <Avatar className="" >{props.entreprise.id_entreprise.slice(0, 1)}</Avatar>
-                <div className={isHover ? `text-red-500 hover:text-white` : ` hidden`} onClick={() => deleteentreprise(props.entreprise.id_entreprise)} >
+                <Avatar className="" >{entreprise.id_entreprise.slice(0, 1)}</Avatar>
+                <div className={isHover ? `text-red-500 hover:text-white` : ` hidden`} onClick={() => deleteentreprise(entreprise.id_entreprise)} >
                     <DeleteIcon /> delete
                 </div>
             </div>
             <div className="flex flex-wrap gap-5 items-center opacity-70" >
                 <p className="text-gray-400 text-sm">id_entreprise:</p>
-                <p className="text-gray-200  ">{props.entreprise.id_entreprise}</p>
+                <p className="text-gray-200  ">{entreprise.id_entreprise}</p>
 
 
             </div>
             <div className="flex flex-wrap gap-4 items-center opacity-70">
                 <p className="text-gray-400 text-sm">username Admin :</p>
-                <p className="text-gray-200  ">{props.entreprise.username_admin}</p>
+                <p className="text-gray-200  ">{entreprise.username_admin}</p>
             </div>
 
             <div className="flex flex-wrap gap-4 items-center opacity-70">
                 <p className="text-gray-400 text-sm">titre:</p>
-                <p className="text-gray-200  ">{props.entreprise.title_entreprise}</p>
+                <p className="text-gray-200  ">{entreprise.title_entreprise}</p>
             </div>
 
             <div className="flex flex-wrap gap-4 items-center opacity-70">
                 <p className="text-gray-400 text-sm">domaine:</p>
-                <p className="text-gray-200  ">{props.entreprise.domaine}</p>
+                <p className="text-gray-200  ">{entreprise.domaine}</p>
             </div>
 
             <div className="flex flex-wrap gap-4 items-center opacity-70">
                 <p className="text-gray-400 text-sm">code_conf:</p>
-                <p className="text-gray-50  ">{props.entreprise.code_conf}</p>
+                <p className="text-gray-50  ">{entreprise.code_conf}</p>
             </div>
 
 
             <div className="flex flex-wrap gap-4 items-center opacity-70">
                 <p className="text-gray-400 text-sm">interests:</p>
-                <p className="text-gray-50  ">{props.entreprise.interests}</p>
+                <p className="text-gray-50  ">{entreprise.interests}</p>
             </div>
 
             <div className="flex flex-wrap gap-4 items-center opacity-70">
                 <p className="text-gray-400 text-sm">description:</p>
-                <p className="text-gray-50  ">{props.entreprise.description}</p>
+                <p className="text-gray-50  ">{entreprise.description}</p>
             </div>
 
 
@@ -86,7 +86,7 @@ export async function getStaticProps() {
     // will receive `posts` as a prop at build time
     return {
         props: {
-            posts,
+            entreprise,
         },
     }
 }
