1bcc3f8fd6e5f5f36e738c14548d2460453a61af
OnceLearner <redaamir972@gmail.com>
2021-01-18 21:24:34 +0100

    test : test 6 think is fixed , but props got damaged from the paretn

--- pages/components/job/CardJob.js
+++ pages/components/job/CardJob.js
@@ -15,40 +15,40 @@ const deleteJob = id_job => {
 }
 
 
-const CardJob = (props) => {
+const CardJob = ({ job }) => {
     const [isHover, setIsHover] = useState(false)
     return (
         <div className="flex flex-col   border border-gray-600 shadow-xl hover:shadow-2xl p-4  md:w-80 flex-wrap rounded-lg space-y-4 bg-gradient-to-tr from-gray-800 to-gray-700  transform cursor-pointer hover:scale-105 transition delay-50  " style={{ fontFamily: "Questrial" }} onMouseEnter={() => setIsHover(true)} onMouseLeave={() => setIsHover(false)} >
             <div className="flex gap-8 items-center justify-center p-3">
-                <Avatar className="" >{props.job.id_job.slice(0, 1)}</Avatar>
-                <div className={isHover ? `text-red-500 hover:text-red-400` : ` hidden`} onClick={() => deleteJob(props.job.id_job)} >
+                <Avatar className="" >{job.id_job.slice(0, 1)}</Avatar>
+                <div className={isHover ? `text-red-500 hover:text-red-400` : ` hidden`} onClick={() => deleteJob(job.id_job)} >
                     <DeleteIcon /> delete
                 </div>
             </div>
             <div className="flex flex-wrap gap-5 items-center opacity-70" >
                 <p className="text-gray-400 text-sm">id_job:</p>
-                <p className="text-gray-200  ">{props.job.id_job}</p>
+                <p className="text-gray-200  ">{job.id_job}</p>
 
 
             </div>
             <div className="flex flex-wrap gap-4 items-center opacity-70">
                 <p className="text-gray-400 text-sm">id entrerprise:</p>
-                <p className="text-gray-200  ">{props.job.id_entreprise}</p>
+                <p className="text-gray-200  ">{job.id_entreprise}</p>
             </div>
 
             <div className="flex flex-wrap gap-4 items-center opacity-70">
                 <p className="text-gray-400 text-sm">titre:</p>
-                <p className="text-gray-200  ">{props.job.title_job}</p>
+                <p className="text-gray-200  ">{job.title_job}</p>
             </div>
 
             <div className="flex flex-wrap gap-4 items-center opacity-70">
                 <p className="text-gray-400 text-sm">description:</p>
-                <p className="text-gray-200  ">{props.job.description_job}</p>
+                <p className="text-gray-200  ">{job.description_job}</p>
             </div>
 
             <div className="flex flex-wrap gap-4 items-center opacity-70">
                 <p className="text-gray-400 text-sm">tags:</p>
-                <p className="text-gray-50  ">{props.job.tags.map(tag => (<p>#{tag}</p>))}</p>
+                <p className="text-gray-50  ">{job.tags.map(tag => (<p>#{tag}</p>))}</p>
             </div>
 
 
@@ -62,5 +62,21 @@ const CardJob = (props) => {
         </div >
     )
 }
+export async function getStaticProps() {
+    // Call an external API endpoint to get posts.
+    // You can use any data fetching library
+    const res = await fetch('https://webrtc-back1.herokuapp.com/job/jobs')
+    const job1 = await res.json()
+    const job = job1[0];
+
+    // By returning { props: posts }, the Blog component
+    // will receive `posts` as a prop at build time
+    return {
+        props: {
+            job,
+        },
+    }
+}
+
 
 export default CardJob
--- pages/components/user/CardUser.js
+++ pages/components/user/CardUser.js
@@ -15,45 +15,45 @@ const deleteUser = username => {
 }
 
 
-const CardUser = (props) => {
+const CardUser = ({ user }) => {
     const [isHover, setIsHover] = useState(false)
     return (
         <div className="flex flex-col   border border-gray-600 shadow-xl hover:shadow-2xl p-4  md:w-80 flex-wrap rounded-lg space-y-4 bg-gradient-to-tr from-gray-800 to-gray-700  transform cursor-pointer hover:scale-105 transition delay-50  " style={{ fontFamily: "Questrial" }} onMouseEnter={() => setIsHover(true)} onMouseLeave={() => setIsHover(false)} >
             <div className="flex gap-8 items-center justify-center p-3">
-                <Avatar className="" >{props.user.username.slice(0, 1)}</Avatar>
-                <div className={isHover ? `text-red-500 hover:text-white` : ` hidden`} onClick={() => deleteUser(props.user.username)} >
+                <Avatar className="" >{user.username.slice(0, 1)}</Avatar>
+                <div className={isHover ? `text-red-500 hover:text-white` : ` hidden`} onClick={() => deleteUser(user.username)} >
                     <DeleteIcon /> delete
                 </div>
             </div>
             <div className="flex flex-wrap gap-5 items-center opacity-70" >
                 <p className="text-gray-400 text-sm">username:</p>
-                <p className="text-gray-200  ">{props.user.username}</p>
+                <p className="text-gray-200  ">{user.username}</p>
 
 
             </div>
             <div className="flex flex-wrap gap-4 items-center opacity-70">
                 <p className="text-gray-400 text-sm">name:</p>
-                <p className="text-gray-200  ">{props.user.name}</p>
+                <p className="text-gray-200  ">{user.name}</p>
             </div>
 
             <div className="flex flex-wrap gap-4 items-center opacity-70">
                 <p className="text-gray-400 text-sm">prenom:</p>
-                <p className="text-gray-200  ">{props.user.prenom}</p>
+                <p className="text-gray-200  ">{user.prenom}</p>
             </div>
 
             <div className="flex flex-wrap gap-4 items-center opacity-70">
                 <p className="text-gray-400 text-sm">ecole:</p>
-                <p className="text-gray-200  ">{props.user.ecole}</p>
+                <p className="text-gray-200  ">{user.ecole}</p>
             </div>
 
             <div className="flex flex-wrap gap-4 items-center opacity-70">
                 <p className="text-gray-400 text-sm">filiere:</p>
-                <p className="text-gray-50  ">{props.user.filiere}</p>
+                <p className="text-gray-50  ">{user.filiere}</p>
             </div>
 
             <div className="flex flex-wrap gap-4 items-center opacity-70">
                 <p className="text-gray-400 text-sm">cv:</p>
-                <p className="text-gray-50  ">{props.user.cv}</p>
+                <p className="text-gray-50  ">{user.cv}</p>
             </div>
 
             <div className=" text-gray-300  text-center space-x-8">
@@ -70,5 +70,21 @@ const CardUser = (props) => {
         </div >
     )
 }
+export async function getStaticProps() {
+    // Call an external API endpoint to get posts.
+    // You can use any data fetching library
+    const res = await fetch('https://webrtc-back1.herokuapp.com/user/etudiant/all')
+    const user1 = await res.json()
+    const user = user1[0];
+
+    // By returning { props: posts }, the Blog component
+    // will receive `posts` as a prop at build time
+    return {
+        props: {
+            user,
+        },
+    }
+}
+
 
 export default CardUser
