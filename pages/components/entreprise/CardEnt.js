59a74bc2a4da6dadceb520d1d9d3de2dce9eb777
OnceLearner <redaamir972@gmail.com>
2021-01-18 21:10:44 +0100

    test : test 1

--- pages/components/entreprise/CardEnt.js
+++ pages/components/entreprise/CardEnt.js
@@ -75,4 +75,24 @@ const CardEnt = (props) => {
     )
 }
 
+
+export async function getStaticProps() {
+    // Call an external API endpoint to get posts.
+    // You can use any data fetching library
+    const res = await fetch('https://webrtc-back1.herokuapp.com/entreprise/entreprises')
+    const posts = await res.json()
+
+    // By returning { props: posts }, the Blog component
+    // will receive `posts` as a prop at build time
+    return {
+        props: {
+            posts,
+        },
+    }
+}
+
+
+
+
+
 export default CardEnt
